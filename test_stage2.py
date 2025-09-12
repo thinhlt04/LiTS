ad5abd9be6f2453a7941cd4daf073edf2126601a
Thinh Luong Truong <thinhlt.uit@gmail.com>
2025-09-12 15:55:32 +0700

    .

--- test_stage2.py
+++ test_stage2.py
@@ -89,9 +89,9 @@ if __name__ == '__main__':
         tumor_mask_np = tumor_mask.cpu().numpy()
         
         batch_opening = []
-        for i in range(tumor_pred.shape[0]):  
-            pred_i = tumor_pred[i, 0]  
-            opening_i = cv2.morphologyEx(pred_i.astype(np.uint8), cv2.MORPH_OPEN, kernel)
+        for i in range(tumor_pred_np.shape[0]):  
+            pred_i = tumor_pred_np[i, 0]  
+            opening_i = cv2.morphologyEx(tumor_pred_np.astype(np.uint8), cv2.MORPH_OPEN, kernel)
             batch_opening.append(opening_i)
 
         batch_opening = np.array(batch_opening)           
